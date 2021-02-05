7fb6601b235118e2545006f64f7540e49f3758b5
Michelle Grounds <mischoel@googlemail.com>
2021-02-05 21:44:14 +0000

    style delete button

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -4,15 +4,26 @@ import { bittersweet, steelTeal } from "../colors";
 
 const DeleteDataContainer = styled.div`
   display: flex;
+  padding: 1em;
+  align-items: center;
 `;
 
 const DeleteDataText = styled.div`
   color: ${steelTeal};
+  padding: 1em;
 `;
 
 const DeleteDataButton = styled.button`
   background: ${bittersweet};
+  width: 120px;
+  height: 40px;
   border-radius: 4px;
+  padding: 1em;
+  color: white;
+  text-align: center;
+  line-height: 1rem;
+  border: none;
+  filter: drop-shadow(0 0 1rem ${bittersweet});
 `;
 
 const deleteData = () => localStorage.clear();
@@ -21,7 +32,7 @@ const Account = () => {
   return (
     <DeleteDataContainer>
       <DeleteDataText>Delete data:</DeleteDataText>
-      <DeleteDataButton onClick={deleteData} />
+      <DeleteDataButton onClick={deleteData}>Delete Data</DeleteDataButton>
     </DeleteDataContainer>
   );
 };
