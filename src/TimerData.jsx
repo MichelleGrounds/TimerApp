47ac2c9176e658e03862ad018a4734a5ff901ceb
Michelle Grounds <mischoel@googlemail.com>
2021-02-05 17:25:01 +0000

    Align items

--- src/TimerData.jsx
+++ src/TimerData.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { aquamarine, bittersweet } from "./colors";
+import { bittersweet, lightCyan, steelTeal } from "./colors";
 import Time from "./Time";
 
 const ButtonContainer = styled.div`
@@ -43,35 +43,59 @@ const getData = (occurrences) => {
 
 const Row = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
   padding: 8px;
 `;
 
 const Cell = styled.div`
   padding: 0.5em 1.5em;
+  flex-basis: 50%;
 `;
 
 const options = {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
+  weekday: "short",
+  year: "2-digit",
+  month: "short",
   day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
 };
+
+const TimerDataText = styled.div`
+  text-align: center;
+  color: ${({ primary }) => (primary ? steelTeal : "black")};
+  font-size: ${({ primary }) => (primary ? "200%" : "75%")};
+`;
+
+const TimerDataHeaderText = styled.div`
+  text-align: center;
+`;
+
+const TimerDataContainer = styled.div`
+  width: 75%;
+  margin: 0 auto;
+`;
 
 const TimerDataLine = ({ data }) => {
   const start = new Date(data.start);
   const stop = new Date(data.stop);
 
-  const startDate = start.toLocaleDateString("en-gb", options);
-  const startTime = start.toISOString().slice(11, 16);
+  const startFormatted = start.toLocaleDateString("en-gb", options);
 
   const duration = stop - start;
 
   return (
     <Row>
-      <Cell>{`${startDate} ${startTime}`}</Cell>
-      <Cell>{Time.convertMS(duration)}</Cell>
-      <Cell>{data.frequency || "-"}</Cell>
+      <Cell>
+        <TimerDataText primary={false}>{startFormatted}</TimerDataText>
+      </Cell>
+      <Cell>
+        <TimerDataText primary={true}>{Time.convertMS(duration)}</TimerDataText>
+      </Cell>
+      <Cell>
+        <TimerDataText primary={false}>{data.frequency || "-"}</TimerDataText>
+      </Cell>
     </Row>
   );
 };
@@ -89,17 +113,23 @@ const TimerData = ({ occurrences }) => {
 
   return (
     <>
-      <div>
+      <TimerDataContainer>
         <Row>
-          <Cell>Start</Cell>
-          <Cell>Duration</Cell>
-          <Cell>Frequency</Cell>
+          <Cell>
+            <TimerDataHeaderText>Start</TimerDataHeaderText>
+          </Cell>
+          <Cell>
+            <TimerDataHeaderText>Duration</TimerDataHeaderText>
+          </Cell>
+          <Cell>
+            <TimerDataHeaderText>Frequency</TimerDataHeaderText>
+          </Cell>
         </Row>
         {data &&
           Object.keys(data)
             .reverse()
             .map((key) => <TimerDataLine data={data[key]} />)}
-      </div>
+      </TimerDataContainer>
     </>
   );
 };
