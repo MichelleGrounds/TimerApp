1809e026ee27c073724abdd46c8afc72ea300a37
Michelle Grounds <mischoel@googlemail.com>
2021-02-06 01:33:23 +0000

    explicitly set theme to dark mode

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,11 @@
 const theme = {
-  aquamarine: "#78FFD6",
-  blackCoffee: "#37323E",
-  steelTeal: "#6E8387",
-  bittersweet: "#FF6B6B",
-  lightCyan: "#E1FAF9",
+  dark: {
+    aquamarine: "#78FFD6",
+    blackCoffee: "#37323E",
+    steelTeal: "#6E8387",
+    bittersweet: "#FF6B6B",
+    lightCyan: "#E1FAF9",
+  },
 };
 
 export default theme;
